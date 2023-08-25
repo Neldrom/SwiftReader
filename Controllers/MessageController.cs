98c2d8335e3c3cc7b9093f73f4f9c62671730c8f
Neldrom <nikimev01@gmail.com>
2023-08-25 21:48:55 +0300

    init commit

---
+++ Controllers/MessageController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Data.Sqlite;
+using System.IO;
+
+namespace SwiftReader.Controllers
+{
+    [Route("api/messages")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private const string _connectionString = "Data Source=Messages.db;";
+
+        [HttpPost("upload")]
+        public IActionResult UploadSwiftFile([FromForm] IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("No file uploaded.");
+            }
+
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                string content = reader.ReadToEnd();
+
+                using (var connection = new SqliteConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = "INSERT INTO Messages (Message) VALUES (@message)";
+                        command.Parameters.AddWithValue("@message", content);
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                return Ok("File uploaded and processed.");
+            }
+        }
+    }
+}
---
+++ FileName.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace YourNamespace
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            // Other service registrations
+            services.AddSingleton<IWebHostEnvironment>(env => env.GetRequiredService<IWebHostEnvironment>());
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // Configure error handling for production
+            }
+
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
